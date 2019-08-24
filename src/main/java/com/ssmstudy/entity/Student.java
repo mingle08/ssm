ebd22979d303d3af821f0c39c4eaf1ed2294a9ab
mingle08 <mingle08@163.com>
2019-08-24 19:44:26 +0800

    自动生成代码

---
+++ src/main/java/com/ssmstudy/entity/Student.java
@@ -0,0 +1,43 @@
+package com.ssmstudy.entity;
+
+public class Student {
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+
+    private Integer cardId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Integer getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(Integer cardId) {
+        this.cardId = cardId;
+    }
+}
