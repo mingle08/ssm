ad281520c3fcc47835ba3f9d09ba0e78f268bfcb
mingleCat <mingle08@163.com>
2022-12-11 22:37:15 +0800

    XMLGregorianCalendar

---
+++ src/main/java/com/ssmstudy/jdk/DateUtil.java
@@ -0,0 +1,30 @@
+package com.ssmstudy.jdk;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class DateUtil {
+    // XMLGregorianCalendar => java.util.Date
+    public static Date convertToDate(XMLGregorianCalendar xmlGregorianCalendar) {
+        GregorianCalendar gregorianCalendar = xmlGregorianCalendar.toGregorianCalendar();
+        return gregorianCalendar.getTime();
+    }
+
+    // java.util.Date => XMLGregorianCalendar
+    public static XMLGregorianCalendar convertToXmlDate(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        XMLGregorianCalendar xmlGregorianCalendar = null;
+        try {
+            xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(gregorianCalendar);
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
+
+        return xmlGregorianCalendar;
+    }
+}
